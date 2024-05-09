30988d3817229e4bb49d1e3196fb5a04875580bb
rafarush <158175139+rafarush@users.noreply.github.com>
2024-05-09 01:32:35 -0400

    Create HolaMundo.java
    comente y agregue package

---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,9 @@
+package HolaMundo;
+
+public class HolaMundo {
+    public static void main (String[] args) {
+      //String naranja = "\033[33m";
+      //String azul = "\033[34m";
+      System.out.println("Hola mundo");
+    }
+  }
