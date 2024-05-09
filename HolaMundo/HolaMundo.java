8874bba922a9cc103158a16f210ac4333fac8de3
rafarush <158175139+rafarush@users.noreply.github.com>
2024-05-09 01:34:06 -0400

    Update HolaMundo.java
    quite lo de naranja y lo azul

--- HolaMundo/HolaMundo.java
+++ HolaMundo/HolaMundo.java
@@ -2,8 +2,6 @@ package HolaMundo;
 
 public class HolaMundo {
     public static void main (String[] args) {
-      //String naranja = "\033[33m";
-      //String azul = "\033[34m";
       System.out.println("Hola mundo");
     }
   }
