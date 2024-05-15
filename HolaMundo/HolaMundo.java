9b6c1dc6a18ba134a1e5773f4894ffb5abe76225
Rafael García González <158175139+rafarush@users.noreply.github.com>
2024-05-14 23:12:44 -0400

    Update HolaMundo.java

--- HolaMundo/HolaMundo.java
+++ HolaMundo/HolaMundo.java
@@ -1,7 +1,7 @@
-package HolaMundo;
+package HelloWorld;
 
-public class HolaMundo {
+public class HelloWorld {
     public static void main (String[] args) {
-      System.out.println("Hola mundo");
+      System.out.println("Hello world");
     }
   }
